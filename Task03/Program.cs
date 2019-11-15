6a5c14fd00d91614613c2aafcd6289b404fdd272
polytence <polytence@gmail.com>
2019-11-15 12:47:04 +0400

    add tast03 and rename first task

---
+++ Task03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Task02(N);
+        }
+        public static void Task02(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (n / 2 == i && n / 2 == j)
+                        Console.Write(" ");
+                    else
+                        Console.Write("*");
+                        
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

bb08ecee1a07daded32bb52317262cc13b9747e0
polytence <polytence@gmail.com>
2019-11-15 12:21:29 +0400

    Add task 01 and 02

---
+++ Epam_Task_00/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam_Task_00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Task01(N);
+        }
+        public static void Task01(int n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                if (i != n)
+                {
+                    Console.Write(i + ",");
+                } else
+                {
+                    Console.Write(i);
+                }
+            }
+        }
+    }
+}
---
+++ Task02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Task02(N);
+        }
+        public static void Task02(int n)
+        {
+            bool Simple = true;
+            for (int i = 2; i < n; i++)
+                if (n % i == 0)
+                {
+                    Simple = false;
+                    break;
+                }
+            if (Simple)
+                Console.Write("Простое");
+            else
+                Console.Write("Не простое");
+        }
+    }
+}
+

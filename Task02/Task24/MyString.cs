9250aef077cffee3e23252a8cc90f5c64973a51d
polytence <polytence@gmail.com>
2019-12-02 14:18:21 +0400

    add Task 24

--- Task02/Task24/MyString.cs
+++ Task02/Task24/MyString.cs
@@ -1,14 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Task24
 {
-    public class MyString
+    class MyString 
     {
-        private char[] Str;
+        private char[] chars;
 
+        public static MyString operator +(MyString str1, MyString str2)
+        {
+            return new MyString(str1.ToString() + str2.ToString());
+        }
+
+        public static bool operator !=(MyString str1, MyString str2)
+        {
+            return str1.GetHashCode() != str2.GetHashCode();
+        }
+
+        public static bool operator ==(MyString str1, MyString str2)
+        {
+            return !(str1 != str2);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public MyString(params char[] symbols)
+        {
+            chars = symbols;
+        }
+
+        public MyString(string str)
+        {
+            chars = str.ToCharArray();
+        }
+
+        public override string ToString()
+        {
+            return new string(chars);
+        }
+
+        public int SearchChar(char a)
+        {
+            if (chars.Contains(a))
+            {
+                return Array.IndexOf(chars, a);
+            }
+            else
+            {
+                Console.WriteLine("Такого символа нет");
+                return -1;
+            }
+        }
+
+        public char[] ToCharArray()
+        {
+            return chars;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
     }
 }
--- Task02/Task24/Program.cs
+++ Task02/Task24/Program.cs
@@ -13,6 +13,10 @@ namespace Task24
         {
             MyString string1 = new MyString(new char[] { 'I', 'l', 'o', 'v', 'e' });
             MyString string2 = new MyString(new char[] { 'C', 'o', 'o', 'k', 'e', 's' });
+            Console.WriteLine(string1 + string2);
+            Console.WriteLine(string1 != string2);
+            Console.WriteLine(string1 == string2);
+            Console.WriteLine(string1.SearchChar('e'));
         }
     }
 }

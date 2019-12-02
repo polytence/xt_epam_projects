f702583829a92f4859e5e1550d05efcc9209db72
polytence <polytence@gmail.com>
2019-12-02 16:57:24 +0400

    Add new classes for 2.7

---
+++ Task02/Task27/Rectangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task27
+{
+    class Rectangle
+    {
+    public int rectangle (int x, int y)
+        {
+            return x * y;
+        }
+
+    }
+}

6359dcab09eb0fcf2b26a100c8940293eb4c3045
polytence <polytence@gmail.com>
2019-12-06 13:37:29 +0400

    Add task3.2

---
+++ Task03/Task32/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello bro. How you men. Iam fine bro. How you. Iam fine too bro. Bro bro bro bro bro";
+            List<string> list = text.ToLower().Split(new char[] { ' ', '.'}).ToList();
+            Print(list);
+
+            Dictionary<string, double> Value = ValueWord(list);
+
+            foreach (var item in Value)
+            {
+                Console.WriteLine("Value word {0}: {1}", item.Key, item.Value);
+            }
+        }
+        static void Print(IEnumerable<string> list)
+        {
+            foreach (var item in list)
+                Console.WriteLine($"{item} ");
+            Console.WriteLine();
+        }
+        static Dictionary<string, double> ValueWord(List<string> list)
+        {
+            Dictionary<string, double> Value = new Dictionary<string, double>();
+            HashSet<string> uniqWords = new HashSet<string>();
+            foreach (string word in list)
+            {
+                uniqWords.Add(word);
+            }
+            foreach (string word in uniqWords)
+            {
+                double value = (double)list.FindAll(t => t == word).Count / list.Count;
+                Value.Add(word, Math.Round(value, 3));
+            }
+            return Value;
+        }
+    }
+}

48df31aba355cff116882445d6fab2c8c3bcb455
polytence <polytence@gmail.com>
2019-12-12 11:57:48 +0400

    Finish task 4.1

---
+++ Task04/Task4/Task41/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task41
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 5, 10, 2, 33, 90, 6 };
+            QuickSort.BubbleSort<int>(array, Swap, Compare);
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Swap(ref int x, ref int y)
+        {
+            var temp = x;
+            x = y;
+            y = temp;
+        }
+        static bool Compare(int a, int b)
+        {
+            if (a > b)
+                return true;
+            else if (a < b)
+                return false;
+            else
+                return false;
+        }
+    }
+    public delegate void Swap<T>(ref T x, ref T y);
+    public delegate bool Compare<T>(T x, T y);
+    //сортировка пузырьком
+    public static class QuickSort
+    {
+        public static void BubbleSort<T>(T[] array, Swap<T> swap, Compare<T> compare)
+        {
+            var len = array.Length;
+            for (var i = 1; i < len; i++)
+            {
+                for (var j = 0; j < len - i; j++)
+                {
+                    if (compare.Invoke(array[j], array[j + 1]))
+                    {
+                        swap.Invoke(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+        }
+
+    }
+}

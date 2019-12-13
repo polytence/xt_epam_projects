673f6631746e4a6ca2ce024eb0257013b46e8284
polytence <polytence@gmail.com>
2019-12-13 14:24:54 +0400

    Finish extensions

---
+++ Task04/Task4/Task44/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 10, 11, 55 };
+            double[] b = { 5.45 };
+            double c = a.Sum() + b.Sum();
+            Console.WriteLine(c);
+        }
+    }
+    public static class Operation
+    {
+        public static int Sum(this int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+    }
+}
+
+    
+
---
+++ Task04/Task45/task45/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "7";
+            Console.WriteLine(str.StrPositive());
+        }
+    }
+    public static class Positive
+    {
+        public static bool StrPositive(this string i) => i.All(char.IsDigit);
+    }
+}
